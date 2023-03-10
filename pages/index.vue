2896ad5f75c73c97359450baf318d134138ffff1
Sitthiphong Achavan <sitthiphong@zompligital.com>
2023-03-10 17:55:03 +0700

    Add example data

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="row">
-    <div>Test</div>
+  <div class="container">
+    <div class="row">
+      <div v-for="(anime, index) in animes" class="col-12" :key="index">
+        <img :src="anime.cover_link" />
+        <div>{{ anime.name_jp }}</div>
+        <div>{{ anime.name_en }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import animesjson from "~/resources/anime.json";
+
+const animes = ref(animesjson);
+</script>
 
 <style lang="scss" scoped></style>
